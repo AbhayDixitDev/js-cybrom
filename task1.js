236107a536f9d2dbc54bebce67837dce5a4dad08
Abhay Dixit <dixitshivam534@gmail.com>
2024-07-06 23:58:17 +0530

    onmouseenter product image show

--- task1.js
+++ task1.js
@@ -1,38 +1,35 @@
-function changeimg1(){
+function changeimg(arg){
+   
+    if(arg===1){
     let store=document.querySelector('#main-2');
-    // let new1=document.querySelector('#photo1');
-    // new1.style.border='1px solid skyblue';
-    // new1.style.backgroundColor='rgb(185, 212, 214)'
     store.style.backgroundImage='url("https://rukminim2.flixcart.com/image/128/128/jdhp47k0/mobile/e/h/e/redmi-note-5-pro-na-original-imaf2ashnnbxxks5.jpeg?q=70&crop=false")';
-    
+    }
+    else if(arg===2){
+        let store=document.querySelector('#main-2');
+        store.style.backgroundImage='url("https://rukminim2.flixcart.com/image/128/128/jrjizrk0/mobile/x/8/3/mi-redmi-note-5-pro-mzb6079in-mzb6087in-original-imafdb6zy2b4hvss.jpeg?q=70&crop=false")';
+    }
+    else if(arg===3){
+        let store=document.querySelector('#main-2');
+        store.style.backgroundImage='url("https://rukminim2.flixcart.com/image/128/128/jdhp47k0/mobile/e/h/e/redmi-note-5-pro-na-original-imaf2ashuwmatzkp.jpeg?q=70&crop=false")';
+    }
+    else if(arg===4){
+        let store=document.querySelector('#main-2');
+        store.style.backgroundImage='url("https://rukminim2.flixcart.com/image/128/128/jdhp47k0/mobile/e/h/e/redmi-note-5-pro-na-original-imaf2ashhyprwwrf.jpeg?q=70&crop=false")';
+    }
+    else if(arg===5){
+        let store=document.querySelector('#main-2');
+        store.style.backgroundImage='url("https://rukminim2.flixcart.com/image/128/128/jddesnk0/mobile/e/h/e/redmi-e7s-note-5-pro-na-original-imaf2ashgckkfqxx.jpeg?q=70&crop=false")';
+    }
+    else if(arg===6){
+        let store=document.querySelector('#main-2');
+        store.style.backgroundImage='url("https://rukminim2.flixcart.com/image/128/128/jdhp47k0/mobile/x/8/3/redmi-note-5-pro-na-original-imaf2asgzugtjaft.jpeg?q=70&crop=false")';
+    }
+    else if(arg===7){
+        let store=document.querySelector('#main-2');
+        store.style.backgroundImage='url("https://rukminim2.flixcart.com/image/128/128/jddesnk0/mobile/x/8/3/redmi-e7s-note-5-pro-na-original-imaf2asknu5jsyc6.jpeg?q=70&crop=false")';
+    }
+
+
 }
-function changeimg2(){
-    let store=document.querySelector('#main-2');
-    store.style.backgroundImage='url("https://rukminim2.flixcart.com/image/128/128/jrjizrk0/mobile/x/8/3/mi-redmi-note-5-pro-mzb6079in-mzb6087in-original-imafdb6zy2b4hvss.jpeg?q=70&crop=false")';
-    
-}
-function changeimg3(){
-    let store=document.querySelector('#main-2');
-    store.style.backgroundImage='url("https://rukminim2.flixcart.com/image/128/128/jdhp47k0/mobile/e/h/e/redmi-note-5-pro-na-original-imaf2ashuwmatzkp.jpeg?q=70&crop=false")';
-    
-}
-function changeimg4(){
-    let store=document.querySelector('#main-2');
-    store.style.backgroundImage='url("https://rukminim2.flixcart.com/image/128/128/jdhp47k0/mobile/e/h/e/redmi-note-5-pro-na-original-imaf2ashhyprwwrf.jpeg?q=70&crop=false")';
-    
-}
-function changeimg5(){
-    let store=document.querySelector('#main-2');
-    store.style.backgroundImage='url("https://rukminim2.flixcart.com/image/128/128/jddesnk0/mobile/e/h/e/redmi-e7s-note-5-pro-na-original-imaf2ashgckkfqxx.jpeg?q=70&crop=false")';
-    
-}
-function changeimg6(){
-    let store=document.querySelector('#main-2');
-    store.style.backgroundImage='url("https://rukminim2.flixcart.com/image/128/128/jdhp47k0/mobile/x/8/3/redmi-note-5-pro-na-original-imaf2asgzugtjaft.jpeg?q=70&crop=false")';
-    
-}
-function changeimg7(){
-    let store=document.querySelector('#main-2');
-    store.style.backgroundImage='url("https://rukminim2.flixcart.com/image/128/128/jddesnk0/mobile/x/8/3/redmi-e7s-note-5-pro-na-original-imaf2asknu5jsyc6.jpeg?q=70&crop=false")';
-    
-}
+
+
