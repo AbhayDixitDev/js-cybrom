8d215184bd8c0c141c8cfb2e8f566e0efe1d945e
Abhay Dixit <dixitshivam534@gmail.com>
2024-07-16 22:18:45 +0530

    for loop with array

---
+++ loop and array.js
@@ -0,0 +1,31 @@
+// let arr = [12,45,67,"deepak", "Arpam", function(){console.log("run function")}];
+
+// console.log(arr[2]);
+
+// console.log(arr[3]);
+
+// console.log(arr[3][1]);
+
+// arr[4]();
+
+// console.log(arr[1]);
+
+// let arr = [12,13,45,78,90,44];
+
+// for(let i = 0; i<arr.length;i++)
+// {
+//     if (arr[i]%2==0)
+//     {
+//         console.log(arr[i])
+//     }
+// }
+
+
+  
+    let arr = [12, 13, 45, 78, 90, 44, "arpan", "sakasham","Abhay"];
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof (arr[i]) === 'string') {
+        console.log(arr[i]);
+      }
+    }
