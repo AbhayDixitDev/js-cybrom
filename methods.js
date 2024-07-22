b4afc52eeb614569cf02bad44a0b9f3fc0c4b101
Abhay Dixit <dixitshivam534@gmail.com>
2024-07-22 17:04:34 +0530

    methods in js

---
+++ methods.js
@@ -0,0 +1,17 @@
+let arr=[34,57,8,9,9,1];
+let arr1=[34,5,80,9,9];
+
+arr.reverse();
+console.log(arr);
+
+let output=arr.join(" ");
+console.log(output);
+
+let output1=arr.toString();
+console.log(output1);
+
+let arr3=arr.concat(arr1);
+console.log(arr3);
+
+let arr4=arr3.slice(0,4);
+console.log(arr4);
