e23ce922ea440e80c5b12728e36ad0061972492c
Abhay Dixit <dixitshivam534@gmail.com>
2024-08-07 16:20:05 +0530

    import & export module

---
+++ module/export.js
@@ -0,0 +1,9 @@
+let num1=56;
+let num2 = 67;
+
+const fun = () =>{
+    console.log("this is funtion call");
+}
+
+export default num1;     //default export
+export {num1,num2,fun};  // named export
---
+++ module/import.js
@@ -0,0 +1,7 @@
+import { num1,num2,fun } from "./export.js";    // named import
+import num1 from "./export.js";    // default import
+
+console.log(num1);
+console.log(num2);
+fun();
+
