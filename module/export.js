d132520f245cba2c3a196b917f0fcbd8e25aa35e
Abhay Dixit <dixitshivam534@gmail.com>
2024-08-07 16:27:43 +0530

    import & export module update

--- module/export.js
+++ module/export.js
@@ -4,6 +4,10 @@ let num2 = 67;
 const fun = () =>{
     console.log("this is funtion call");
 }
+const run = () =>{
+    console.log("this is run funtion call");
+}
+
 
 export default num1;     //default export
-export {num1,num2,fun};  // named export
+export {num1,num2,fun, run};  // named export
--- module/import.js
+++ module/import.js
@@ -1,7 +1,8 @@
-import { num1,num2,fun } from "./export.js";    // named import
-import num1 from "./export.js";    // default import
+import { num1,num2,fun,run } from "./export.js";    // named import
+// import num1 from "./export.js";    // default import
 
 console.log(num1);
 console.log(num2);
 fun();
+run();
 
